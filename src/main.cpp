33b6164f27be64865cede1c521b441a09f5cf188
bdikbas <bdikbas@ucsd.edu>
2026-01-07 18:25:47 -0800

    First Name done

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,55 @@
 #include <Arduino.h>
-constexpr int BUTTON_PIN = 0;
 constexpr int LED_PIN = 13;
+
+constexpr int DOT_TIME = 300;
+constexpr int DASH_TIME = 900;
+constexpr int PAUSE = 300;
+constexpr int LETTER_PAUSE = 900;
+constexpr int WORD_PAUSE = 2000;
+
 // put function declarations here:
 
+void dot() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(DOT_TIME);
+  digitalWrite(LED_PIN,LOW);
+  delay(PAUSE);
+}
+
+void dash() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(DASH_TIME);
+  digitalWrite(LED_PIN,LOW);
+  delay(PAUSE);
+}
+
+const char* morseFirstName[] = {
+  "-...", "..-", ".-.", ".-", "-.-"
+};
+
+void letters(const char* pattern) {
+  for(int i = 0; pattern[i] != '\0'; i++) {
+    if(pattern[i] == '.') {
+      dot();
+    }
+    else if(pattern[i] == '-') {
+      dash();
+    }
+  }
+  (LETTER_PAUSE);
+}
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  pinMode(LED_PIN,OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(digitalRead(BUTTON_PIN) == LOW) {
-    Serial.println("Button is pressed!");
-    digitalWrite(LED_PIN, HIGH);
+  for(int i = 0; i < 5; i++) {
+    letters(morseFirstName[i]);
   }
-  else if( digitalRead(BUTTON_PIN) == HIGH) {
-    digitalWrite(LED_PIN, LOW);
-  }
+  delay(WORD_PAUSE);
 }
 
 
